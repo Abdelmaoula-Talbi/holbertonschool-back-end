52bea49d2323d5e168c6f0e5d5bb5581a2f0dc0e
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2023-05-31 15:05:00 +0100

    fix the code to get all the data

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -12,25 +12,22 @@ if __name__ == "__main__":
     elif argv[1].isdigit() is False:
         print("argument must be an integer")
 
-    my_dict = {}
     my_list = []
     response1 = requests.get("https://jsonplaceholder.typicode.com/users")
 
     for i in response1.json():
         if i.get('id') == int(argv[1]):
-            my_dict['USERNAME'] = i.get('username')
+            USERNAME = i.get('username')
 
     response2 = requests.get("https://jsonplaceholder.typicode.com/todos")
 
     for j in response2.json():
         if j.get('userId') == int(argv[1]):
-            my_dict['USERID'] = j.get('userId')
-            my_dict['COMPLETED_STATUS'] = j.get('completed')
-            my_dict['TITLE'] = j.get('title')
-            my_list.append(my_dict)
-    fields = ['USERID', 'USERNAME', 'COMPLETED_STATUS', 'TITLE']
+            listing = [j.get('userId'), USERNAME,
+                       j.get('completed'), j.get('title')]
+            my_list.append(listing)
 
     with open(f'{argv[1]}.csv', 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=fields, quoting=csv.QUOTE_ALL)
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 
         writer.writerows(my_list)
